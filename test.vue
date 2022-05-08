3ea8c3c0ee0cccac0431403fc6fe74c4ad167f24
mawarutuco <xul4841268@gmail.com>
2022-05-08 21:21:23 +0800

    5/8 練習

---
+++ test.vue
@@ -0,0 +1,38 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        newTodoText: "",
+        todos: [
+          {
+            id: 1,
+            title: "Do the dishes",
+          },
+        ],
+        nextTodoId: 2,
+      };
+    },
+    methods: {
+      addNewTodo() {
+        this.todos.push({
+          id: this.nextTodoId++,
+          title: this.newTodoText,
+        });
+        this.newTodoText = "";
+      },
+    },components:{
+      'hyo':{
+        template:'123'
+      }
+    }
+  });
+  app.component("todo-item", {
+    template: `
+      <li>
+        {{ title }}123456
+        <button @click="$emit('remove')">Remove</button>
+      </li>
+    `,
+    props: ["title"],
+    emits: ["remove"],
+  });
+  app.mount("#todo-list-example");
